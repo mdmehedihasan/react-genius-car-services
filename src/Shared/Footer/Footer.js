0e790b0b554a486ac017ec0140b9558decab71d5
mehedi hasan <mehedicsebu@gmail.com>
2022-04-25 00:11:00 +0600

    react bootstrap implemented

--- src/Shared/Footer/Footer.js
+++ src/Shared/Footer/Footer.js
@@ -4,8 +4,8 @@ const Footer = () => {
     return (
         <div>
 
-            <footer>
-                <p><small>Copyright all {new Date().getFullYear()}</small></p> {/* Outputs 2020 */}
+            <footer className='mt-5'>
+                <p className='fs-5'><small>Copyright all {new Date().getFullYear()}</small></p> {/* Outputs 2020 */}
             </footer>
         </div>
     );
