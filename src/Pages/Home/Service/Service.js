16848fc29e9f95f2804d37866435c2cba506de50
mehedi hasan <mehedicsebu@gmail.com>
2022-05-06 17:28:50 +0600

    server related work done

--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -3,7 +3,7 @@ import { useNavigate } from 'react-router-dom';
 import "../Service/Service.css"
 
 const Service = ({ service }) => {
-    const { name, img, description, price, id } = service;
+    const { name, img, description, price, _id } = service;
     const navigate = useNavigate();
     const navigateToServiceDetail = id => {
         navigate(`/service/${id}`)
@@ -14,7 +14,7 @@ const Service = ({ service }) => {
             <h2>name:{name}</h2>
             <p>{description}</p>
             <p>Price:{price}</p>
-            <button onClick={() => navigateToServiceDetail(id)}>Book Now</button>
+            <button onClick={() => navigateToServiceDetail(_id)}>Book Now</button>
         </div>
     );
 };
--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const { serviceId } = useParams();
+    const [service, setService] = useState({});
+
+    useEffect(() => {
+        const url = `http://localhost:5000/service/${serviceId}`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setService(data));
+
+    }, [])
     return (
         <div>
-            <h3>Welcome to details:{serviceId}</h3>
+            <h3>Welcome to details:{service.name}</h3>
             <div>
                 <Link to="/checkout">
                     <button className='btn btn-primary'>Checkout</button>
